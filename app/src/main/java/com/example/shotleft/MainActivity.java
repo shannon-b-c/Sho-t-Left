1e54cbe4f61f3451e72487c90a76f7532e46ee8f
zmaphalala <zandisiwemaphalala5@gmail.com>
2025-09-05 08:47:11 +0200

    Implemented swoosh animation

--- app/src/main/java/com/example/shotleft/MainActivity.java
+++ app/src/main/java/com/example/shotleft/MainActivity.java
@@ -2,14 +2,9 @@ package com.example.shotleft;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import android.view.animation.PathInterpolator;
+
 import android.widget.ImageView;
-import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Handler;
 
